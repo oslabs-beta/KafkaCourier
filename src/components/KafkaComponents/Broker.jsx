bd70eb5498c843f144d8327ab93ba2b99bdb3fd1
Lilianne Mueller <lilyanneom@gmail.com>
2023-05-30 12:32:58 -0700

    features coming soon added, unique keys on consumer info and group added

--- src/components/KafkaComponents/Broker.jsx
+++ src/components/KafkaComponents/Broker.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './KafkaComponents.scss';
 
-export default function Consumer() {
+export default function Broker() {
   return <div className="coming-soon">Feature Coming Soon!</div>;
 }
--- src/components/KafkaComponents/Producer.jsx
+++ src/components/KafkaComponents/Producer.jsx
@@ -1,7 +1,6 @@
-import React from 'react'
+import React from 'react';
+import './KafkaComponents.scss';
 
 export default function Producer() {
-  return (
-    <div>Producer</div>
-  )
+  return <div className="coming-soon">Feature Coming Soon!</div>;
 }
