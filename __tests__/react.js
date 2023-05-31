7f2a6397252a05ca67bc426b6e744ee6c4b877ad
Jeffrey Huang <jeffuhuang@yahoo.com>
2023-05-31 01:09:16 -0700

    add incomplete tests for dashboard component

--- __tests__/react.js
+++ __tests__/react.js
@@ -5,6 +5,7 @@ import { GoogleOAuthProvider } from '@react-oauth/google';
 
 import Login from '../src/components/Login/Login.js';
 import NavBar from '../src/components/NavBar/NavBar.jsx';
+import Dashboard from '../src/components/dashboard/Dashboard.jsx';
 
 describe('Unit testing React components', () => {
 
@@ -21,15 +22,35 @@ describe('Unit testing React components', () => {
   });
 
   describe('NavBar component', () => {
-    test('renders without error', () => {
-      const navBar = render(
+    test('Renders without error', () => {
+      const { container } = render(
         <BrowserRouter>
           <NavBar />
         </BrowserRouter>
       );
-      const element = document.getElementsByClassName('navBar');
+      const element = document.getElementsByClassName('navBar').length;
       expect(element.length).toBe(1);
     });
   });
+
+  // describe('Dashboard component', () => {
+  //   const dashboard = render(
+  //     <BrowserRouter>
+  //       <Dashboard />
+  //     </BrowserRouter>
+  //   );
+
+  //   // console.log('dashboard: ', dashboard);
+  //   // console.log('getByText: ', dashboard.getByText('Topics'));
+  //   // console.log('getByClass: ', document.getElementsByClassName('navBar'))
+
+  //   test('Renders NavBar component', () => {
+  //     expect(container.getElementsByClassName('navBar').length).toBe(1);
+  //   })
+
+  //   test('Renders KafkaContainer component', () => {
+  //     expect(container.getElementsByClassName('kafka-container').length).toBe(1);
+  //   })
+  // })
 
 });
