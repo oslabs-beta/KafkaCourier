c661e1e41c0ce4cd2784b9509925692026ede284
Jeffrey Huang <jeffuhuang@yahoo.com>
2023-06-01 09:47:27 -0700

    fix styling

--- src/components/KafkaComponents/ConsumptionRateContainer.jsx
+++ src/components/KafkaComponents/ConsumptionRateContainer.jsx
@@ -6,16 +6,10 @@ import ConsumptionRate from './ConsumptionRate.jsx';
 
 export default function ConsumptionRateContainer( {currentTopic, consumerGroup}) {
   return (
-    <div id="consumption-rate" className="card-container">
+    <div className="card-container">
       <>
         <h4>Consumption Rate</h4>
-        <Card>
-        <CardContent>
-            <CardMedia>
-            <ConsumptionRate currentTopic={currentTopic} consumerGroup={consumerGroup}></ConsumptionRate>
-            </CardMedia>
-        </CardContent>
-        </Card>
+        <ConsumptionRate currentTopic={currentTopic} consumerGroup={consumerGroup}></ConsumptionRate>
       </>
     </div>
   )
